233249738f5ef9b614cd356392141da6add5bed9
Shadi <tobia.shadi@gmail.com>
2025-09-25 15:40:48 +0300

    Update Firebase config, modify .gitignore, and remove unused folder

--- src/utility/firebaseConfig.js
+++ src/utility/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDy66Z9-1kP90nQMB_WgxLQhs5YK39Ipag',
-  authDomain: 'baytak-14484.firebaseapp.com',
-  projectId: 'baytak-14484',
-  storageBucket: 'baytak-14484.firebasestorage.app',
-  messagingSenderId: '138112039953',
-  appId: '1:138112039953:web:ebd25eb6393052dae585b0',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
